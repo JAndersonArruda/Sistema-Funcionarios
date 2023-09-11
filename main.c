8cbb3125b163d10a3d13d4a54bddb7731783f7da
Jose Anderson Arruda <ferrazarrudaanderson@gmail.com>
2023-09-11 19:24:58 -0300

    ajustando estrutura

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 -- Criar um sistema continuo.
 */
 
+//obs: colocar pausa de segurança no fim de cada ação para que o ususario veja as informações, antes de iniciar novamente o menu.
 
 #include <stdio.h>
 
@@ -18,38 +19,51 @@ void main() {
     int acao;
 
     while (1) {
-        printf(" --------- Sistema de Funcionarios ---------- \n\n");
-        printf("\t---------- Menu ----------- \n");
-        printf("\t01. Cadastrar...\n");
-        printf("\t02. Atualizar...\n");
-        printf("\t03. Buscar...\n");
-        printf("\t04. Ler...\n");
-        printf("\t05. Remover...\n");
-        printf("\t06. Encerrar sessão...\n\n");
+        printf("\n ---------- Sistema de Funcionarios ---------- \n\n");
 
-        printf(" Por favor digite o indice da operação que deseja realizar: ");
+        printf("\n\t------------ Menu ------------- \n\n");
+        printf("\t   01. Cadastrar...\n");
+        printf("\t   02. Atualizar...\n");
+        printf("\t   03. Buscar...\n");
+        printf("\t   04. Ler...\n");
+        printf("\t   05. Remover...\n");
+        printf("\t   06. Encerrar sessão...\n\n");
+        printf("\t------------------------------- \n\n");
+
+        printf("system log: Por favor digite o indice da operação que deseja realizar: ");
         scanf("%d", &acao);
+
         if (acao == 01 || acao == 1) {
             printf("\n---------- Ação de Cadastro! ----------\n");
+            printf("system log: preencha os campos necessarios para o cadastro...\n");
+            //chamar procedimento createEmployee();
         }
         else if (acao == 02 || acao == 2) {
-           printf("\n---------- Ação de Atualização! ----------\n");
+            printf("\n---------- Ação de Atualização! ----------\n");
+            printf("system log: preencha os campos necessarios para a atualização dos dados do funcionario...\n");
+            //chamar procedimento udateEmployee();
         }
         else if (acao == 03 || acao == 3) {
             printf("\n---------- Ação de Busca! ----------\n");
+            printf("system log: preencha os campos necessarios para a busca de funcionarios...\n");
+            //chamar procedimento fetchEmployee();
         }
         else if (acao == 04 || acao == 4) {
-            printf("\n---------- Ação de Leitura! ----------\n");
+            printf("\n---------- Ação de Leitura! ----------\n\n");
+            //chamar procedimento readEmployee();
+            printf("system log: leitura completa.\n");
         }
         else if (acao == 05 || acao == 5) {
             printf("\n---------- Ação de Remoção! ----------\n");
+            printf("system log: preencha os campos necessarios para a remoção...\n");
+            //chamar procedimento deleteEmployee();
         }
         else if (acao == 06 || acao == 6) {
             break;
         }
         else {
-            printf("\nInforme uma ação valida!\n\n");
+            printf("\nsystem log: Informe uma ação valida!\n\n");
         }
     }
-    printf("\nsistem log: Sistema Encerrado! \n");
+    printf("\nsystem log: Sistema Encerrado! \n");
 }

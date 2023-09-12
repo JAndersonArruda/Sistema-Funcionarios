dd95dc653fb01eddb46592fef9f7ca20a05041fc
Luiz Fernando <luizfernandoinfo100@gmail.com>
2023-09-12 14:57:07 -0300

    feat: ajustando menu.

--- system-actions.h
+++ system-actions.h
@@ -25,13 +25,13 @@ void closeFile(FILE *file) {
 int menu() {
     int acao;
     
-    printf("\n\t------------ Menu ------------ \n\n");
-    printf("\t   [1] Cadastrar...\n");
-    printf("\t   [2] Atualizar...\n");
-    printf("\t   [3] Buscar...\n");
-    printf("\t   [4] Ler...\n");
-    printf("\t   [5] Remover...\n");
-    printf("\t   [6] Encerrar sessão...\n\n");
+    printf("\n\t------------ Menu ------------- \n\n");
+    printf("\t   [1] Cadastrar Funcionario\n");
+    printf("\t   [2] Atualizar Dados\n");
+    printf("\t   [3] Buscar Funcionario\n");
+    printf("\t   [4] Ler\n");
+    printf("\t   [5] Remover Funcionario\n");
+    printf("\t   [6] Encerrar sessão\n\n");
     printf("\t------------------------------- \n\n");
 
     printf("system log: Por favor digite o indice da operação que deseja realizar: ");

db6570294a97934235ee8e3a11ed597317fc5f3e
Jose Anderson Arruda <ferrazarrudaanderson@gmail.com>
2023-09-12 14:29:09 -0300

    ajustes no main

--- main.c
+++ main.c
@@ -34,37 +34,37 @@ void main() {
         // scanf("%d", &acao);
         getchar();
 
-        if (acao == 01 || acao == 1) {
+        if (acao == 1) {
             printf("\n---------- Ação de Cadastro! ----------\n");
             printf("system log: preencha os campos necessarios para o cadastro...\n");
             //chamar procedimento createEmployee();
             getchar();
         }
-        else if (acao == 02 || acao == 2) {
+        else if (acao == 2) {
             printf("\n---------- Ação de Atualização! ----------\n");
             printf("system log: preencha os campos necessarios para a atualização dos dados do funcionario...\n");
             //chamar procedimento udateEmployee();
             getchar();
         }
-        else if (acao == 03 || acao == 3) {
+        else if (acao == 3) {
             printf("\n---------- Ação de Busca! ----------\n");
             printf("system log: preencha os campos necessarios para a busca de funcionarios...\n");
             //chamar procedimento fetchEmployee();
             getchar();
         }
-        else if (acao == 04 || acao == 4) {
+        else if (acao == 4) {
             printf("\n---------- Ação de Leitura! ----------\n\n");
             //chamar procedimento readEmployee();
             printf("system log: leitura completa.\n");
             getchar();
         }
-        else if (acao == 05 || acao == 5) {
+        else if (acao == 5) {
             printf("\n---------- Ação de Remoção! ----------\n");
             printf("system log: preencha os campos necessarios para a remoção...\n");
             //chamar procedimento deleteEmployee();
             getchar();
         }
-        else if (acao == 06 || acao == 6) {
+        else if (acao == 6) {
             break;
         }
         else {

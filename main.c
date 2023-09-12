40d4dcd2297f5e8b91702decb0f069a631fb891a
Luiz Fernando <luizfernandoinfo100@gmail.com>
2023-09-12 14:53:00 -0300

    feat: deixando main semantico e ajustando o cadastramento

--- main.c
+++ main.c
@@ -1,77 +1,83 @@
-/*
--- Criar um sistema de funcionarios em que as informações sejam amrazenadas em um arquivo .csv
--- Cada dado deve ser armazenado de formma organizada, cada coluna deve conter um unico tipo de informação
--- Os referentes dados são:
-    -->CPF(id);
-    -->Nome;
-    -->Endereço;
-    -->Idade;
-    -->Cargo;
-
--- Criar um sistema continuo.
-*/
-
-
 #include <stdio.h>
+#include <stdlib.h>
 #include "system-actions.h"
 
 void main() {
+    Funcionario funcionarios[3];
+
+    int idxFuncionario = 0;
     int acao;
 
-    while (1) {
-        printf("\n ---------- Sistema de Funcionarios ---------- \n\n");
+    printf("\n ---------- Sistema de Funcionarios ---------- \n\n");
+    do {
+        acao = menu();
 
-        int acao = menu();
-        // printf("\n\t------------ Menu ------------- \n\n");
-        // printf("\t   01. Cadastrar...\n");
-        // printf("\t   02. Atualizar...\n");
-        // printf("\t   03. Buscar...\n");
-        // printf("\t   04. Ler...\n");
-        // printf("\t   05. Remover...\n");
-        // printf("\t   06. Encerrar sessão...\n\n");
-        // printf("\t------------------------------- \n\n");
-        // printf("system log: Por favor digite o indice da operação que deseja realizar: ");
-        // scanf("%d", &acao);
-        getchar();
+        switch (acao) {
+            case 1:
+                printf("\n---------- Cadastre-se! ----------\n");
+                printf("system log: preencha os campos necessarios para o cadastro...\n");
 
-        if (acao == 01 || acao == 1) {
-            printf("\n---------- Ação de Cadastro! ----------\n");
-            printf("system log: preencha os campos necessarios para o cadastro...\n");
-            //chamar procedimento createEmployee();
-            getchar();
+                char nome[50], cpf[15], endereco[100];
+                int idade;
+                float salario;
+
+                fflush(stdin);
+                printf("Nome: ");
+                scanf("%s", &nome);
+                fflush(stdin);
+                printf("Idade: ");
+                scanf("%d", &idade);
+                fflush(stdin);
+                printf("CPF: ");
+                scanf("%s", &cpf);
+                fflush(stdin);
+                printf("Endereço: ");
+                scanf("%s", &endereco);
+                fflush(stdin);
+                printf("Salário: ");
+                scanf("%f", &salario);
+                fflush(stdin);
+
+                if (idxFuncionario < 3) {
+                    createEmployee(&funcionarios, idxFuncionario, nome, idade, cpf, endereco, salario);
+                    idxFuncionario++;
+                } else {
+                    writeFile(funcionarios, "funcionarios.csv");
+                }
+                break;
+            case 2:
+                printf("\n---------- Ação de Atualização! ----------\n");
+                printf("system log: preencha os campos necessarios para a atualização dos dados do funcionario...\n");
+                //chamar procedimento udateEmployee();
+                getchar();
+                break;
+            case 3:
+                printf("\n---------- Ação de Busca! ----------\n");
+                printf("system log: preencha os campos necessarios para a busca de funcionarios...\n");
+                //chamar procedimento fetchEmployee();
+                getchar();
+                break;
+            case 4:
+                printf("\n---------- Ação de Leitura! ----------\n\n");
+                //chamar procedimento readEmployee();
+                printf("system log: leitura completa.\n");
+                getchar();
+                break;
+            case 5:
+                printf("\n---------- Ação de Remoção! ----------\n");
+                printf("system log: preencha os campos necessarios para a remoção...\n");
+                //chamar procedimento deleteEmployee();
+                getchar();
+                break;
+            case 6:
+                continue;
+            default:
+                printf("\nsystem log: Informe uma ação valida!\n\n");
+                getchar();
+                break;
         }
-        else if (acao == 02 || acao == 2) {
-            printf("\n---------- Ação de Atualização! ----------\n");
-            printf("system log: preencha os campos necessarios para a atualização dos dados do funcionario...\n");
-            //chamar procedimento udateEmployee();
-            getchar();
-        }
-        else if (acao == 03 || acao == 3) {
-            printf("\n---------- Ação de Busca! ----------\n");
-            printf("system log: preencha os campos necessarios para a busca de funcionarios...\n");
-            //chamar procedimento fetchEmployee();
-            getchar();
-        }
-        else if (acao == 04 || acao == 4) {
-            printf("\n---------- Ação de Leitura! ----------\n\n");
-            //chamar procedimento readEmployee();
-            printf("system log: leitura completa.\n");
-            getchar();
-        }
-        else if (acao == 05 || acao == 5) {
-            printf("\n---------- Ação de Remoção! ----------\n");
-            printf("system log: preencha os campos necessarios para a remoção...\n");
-            //chamar procedimento deleteEmployee();
-            getchar();
-        }
-        else if (acao == 06 || acao == 6) {
-            break;
-        }
-        else {
-            printf("\nsystem log: Informe uma ação valida!\n\n");
-            getchar();
-        }
-    }
+    } while (acao != 6);
+    
     printf("\nsystem log: Sistema Encerrado! \n");
     getchar();
 }

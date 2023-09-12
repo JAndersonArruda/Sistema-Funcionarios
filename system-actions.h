baacf4019f2127ac2bb86c59727fbbd912d673c1
caua-3301 <134548536+caua-3301@users.noreply.github.com>
2023-09-12 11:09:35 -0300

    adicionando procedimento de leitura de arquivo

--- system-actions.h
+++ system-actions.h
@@ -32,7 +32,8 @@ int menu() {
 }
 
 Funcionario createEmployee(char nome[50], int idade, char cpf[15], char endereco[100], float salario) {
-    Funcionario *funcionario = malloc(sizeof(Funcionario));
+	//Seguestão de mudança ---
+    Funcionario *funcionario = (Funcionario *)malloc(sizeof(Funcionario));
     funcionario->id = 1;
     strcpy(funcionario->nome, nome);
     funcionario->idade = idade;
@@ -41,4 +42,34 @@ Funcionario createEmployee(char nome[50], int idade, char cpf[15], char endereco
     funcionario->salario = salario;
 
     return *funcionario;
+}
+
+//Recebe o nome do arquivo desejado (caso esteja em pasta diferente, enviar o caminho do arquivo)
+void readEmployee(char fileName[50]){
+	
+	FILE* file = fopen(fileName, "r");
+	if (!file){
+		printf ("Impossível abrir arquivo");
+	}
+	else{
+		
+		//String responsável por armazenar os valores do documento
+		char buffer[1024];	
+		
+		//Enquanto o documento não estiver nulo
+		while(fgets(buffer, 1024, file)){
+			
+			//Definindo a separação quando uma vírgula for identificads
+			char * value = strtok(buffer, ", ");			
+			//Enquanto aquela linha não for nula, percorra
+			while(value){
+				printf("%s  ", value);
+                value = strtok(NULL, ", ");
+			}	
+			//Qubrando entre uma linha e outra
+			printf("\n");
+		}
+		//Encerrando execução do arquivo
+		fclose(file);
+	}	
 }

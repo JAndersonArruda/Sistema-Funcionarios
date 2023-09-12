655c7ad9d710d84aa541712b95420be7c0918ee3
Luiz Fernando <luizfernandoinfo100@gmail.com>
2023-09-11 22:11:09 -0300

    adicionando registro Funcionario.

--- system-actions.h
+++ system-actions.h
@@ -1,2 +1,14 @@
-// #include <stdio.h>
-// #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct {
+    int id;
+    char nome[50];
+    int idade;
+    char cpf[15];
+    char endereco[100];
+    float salario;
+} Funcionario;
+

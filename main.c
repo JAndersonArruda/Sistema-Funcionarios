d426606240814c2db4055a06bede79958e85b14b
caua-3301 <134548536+caua-3301@users.noreply.github.com>
2023-10-03 12:08:35 -0300

    Adicao da opcao criar arquivo, otimizacao em exibir lista, agora funcionarios estao sendo adicionados em um documento da escolha do usuario e melhorias gerais

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "system-actions.h"
 
+char fileName[50];
+
 void main() {
     Funcionario funcionarios[3];
 
@@ -13,20 +15,32 @@ void main() {
         acao = menu();
 
         switch (acao) {
-            case 1:
+        	
+        	case 1:
+                printf("\n---------- A��o de criar um arquivo! ----------\n");
+                printf("system log: Informe o nome do arquivo que ser� criado para realizar as devidas opera��es...\n");
+                gets(fileName);
+                //chamar fun��o createFile();
+                //Declarando o arquivo aqui para utilizar apenas ele no decorrer da execu��o
+                openFIle(fileName, "w");
+                getchar();
+                break;
+        	
+            case 2:
                 printf("\n---------- Cadastre-se! ----------\n");
-                printf("system log: preencha os campos necessarios para o cadastro...\n");
+                printf("system log: preencha o nome do aruivo e os campos necessarios para o cadastro...\n");
+                printf ("Nome do arquivo: \n");
+                scanf ("%s", &fileName);
 
-                char nome[50], cpf[15], endereco[100];
-                int idade;
-                float salario;
+                char nome[50], cpf[15], endereco[100], idade[5], salario[15];
+
 
                 fflush(stdin);
                 printf("Nome: ");
                 scanf("%s", &nome);
                 fflush(stdin);
                 printf("Idade: ");
-                scanf("%d", &idade);
+                scanf("%s", &idade);
                 fflush(stdin);
                 printf("CPF: ");
                 scanf("%s", &cpf);
@@ -35,51 +49,48 @@ void main() {
                 scanf("%s", &endereco);
                 fflush(stdin);
                 printf("Salário: ");
-                scanf("%f", &salario);
+                scanf("%s", &salario);
                 fflush(stdin);
 
-                if (idxFuncionario < 3) {
-                    createEmployee(&funcionarios, idxFuncionario, nome, idade, cpf, endereco, salario);
-                    
-                    if (idxFuncionario == 2) {
-                    writeFile(funcionarios, "funcionarios.csv");
-                    }
+                createEmployee(funcionarios, idxFuncionario, nome, idade, cpf, endereco, salario, fileName);
 
-                    idxFuncionario++;
-                }
+                idxFuncionario++;
                 break;
-            case 2:
+            case 3:
                 printf("\n---------- Ação de Atualização! ----------\n");
                 printf("system log: preencha os campos necessarios para a atualização dos dados do funcionario...\n");
                 //chamar procedimento udateEmployee();
                 getchar();
                 break;
-            case 3:
+            case 4:
                 printf("\n---------- Ação de Busca! ----------\n");
                 printf("system log: preencha os campos necessarios para a busca de funcionarios...\n");
                 //chamar procedimento fetchEmployee();
                 getchar();
                 break;
-            case 4:
+            case 5:
                 printf("\n---------- Ação de Leitura! ----------\n\n");
                 //chamar procedimento readEmployee();
+                printf ("Informe o nome do arquivo a ser lido: \n");
+                scanf ("%s", &fileName);
+                readEmployee(fileName);
                 printf("system log: leitura completa.\n");
                 getchar();
                 break;
-            case 5:
+            case 6:
                 printf("\n---------- Ação de Remoção! ----------\n");
                 printf("system log: preencha os campos necessarios para a remoção...\n");
                 //chamar procedimento deleteEmployee();
                 getchar();
                 break;
-            case 6:
+            case 7:
                 continue;
             default:
                 printf("\nsystem log: Informe uma ação valida!\n\n");
                 getchar();
                 break;
         }
-    } while (acao != 6);
+    } while (acao != 7);
     
     printf("\nsystem log: Sistema Encerrado! \n");
     getchar();
--- system-actions.h
+++ system-actions.h
@@ -4,15 +4,15 @@
 
 
 typedef struct {
-    char nome[50];
-    int idade;
+    char nome[60];
+    char idade[5];
     char cpf[15];
     char endereco[100];
-    float salario;
+    char salario[15];
 } Funcionario;
 
 FILE *openFIle(char nome[25], char mode[5]) {
-    FILE *file;
+    FILE * file;
     file = fopen(nome, mode);
 
     return file;
@@ -25,67 +25,108 @@ void closeFile(FILE *file) {
 int menu() {
     int acao;
     
+    //Nova opcao de criar arquivo adicionada
     printf("\n\t------------ Menu ------------- \n\n");
-    printf("\t   [1] Cadastrar Funcionario\n");
-    printf("\t   [2] Atualizar Dados\n");
-    printf("\t   [3] Buscar Funcionario\n");
-    printf("\t   [4] Ler\n");
-    printf("\t   [5] Remover Funcionario\n");
-    printf("\t   [6] Encerrar sessão\n\n");
+    printf("\t   [1] Criar novo arquivo\n");
+    printf("\t   [2] Cadastrar Funcionario (em arquivo existente)\n");
+    printf("\t   [3] Atualizar Dados\n");
+    printf("\t   [4] Buscar Funcionario\n");
+    printf("\t   [5] Ler\n");
+    printf("\t   [6] Remover Funcionario\n");
+    printf("\t   [7] Encerrar sessão\n\n");
     printf("\t------------------------------- \n\n");
 
+	scanf ("%d", &acao);
+
     printf("system log: Por favor digite o indice da operação que deseja realizar: ");
-    scanf("%d", &acao);
+    
 
     return acao;
 }
 
-void createEmployee(Funcionario funcionarios[3], int idx, char name[50], int age, char cpf[15], char address[100], float salary) {
+
+void createEmployee(Funcionario funcionarios[3], int idx, char name[50], char age[5], char cpf[25], char address[100], char salary[15], char fileName[25]) {
 	//Seguest�o de mudan�a ---
     Funcionario *funcionario = (Funcionario *)malloc(sizeof(Funcionario));
-
+    
+    FILE * myFile = openFIle(fileName, "a");
+    
+    if (myFile == NULL) {
+    	printf ("Arquivo nao encotrado\n");
+	}
+	else{
+		
+	//Adicionado o funcion�rio em um documento
     strcpy(funcionario->nome, name);
-    funcionario->idade = age;
+    fputs(funcionario -> nome, myFile);
+    
+    strcpy(funcionario->idade, age);
+    fputs(funcionario -> idade, myFile);
+    
     strcpy(funcionario->cpf, cpf);
+    fputs(funcionario -> cpf, myFile);
+    
     strcpy(funcionario->endereco, address);
-    funcionario->salario = salary;
+    fputs(funcionario -> endereco, myFile);
+    
+    strcpy(funcionario->salario, salary);
+    fputs(funcionario -> salario, myFile);
+
 
     printf("Index: %d", idx);
     funcionarios[idx] = *funcionario;
+		
+	}
+	
+	fclose(myFile);
+    
 }
 
-//Recebe o nome do arquivo desejado (caso esteja em pasta diferente, enviar o caminho do arquivo)
+//Recebe o nome do arquivo desejado (caso esteja em pasta diferente, enviar o caminho do arquivo), imprime todo o arquivo
 void readEmployee(char fileName[50]){
 	
-	FILE* file = fopen(fileName, "r");
+	//Vetor de strings para melhor compreencao na exibicao do arquivo
+	
+	char titles[5][10];
+	strcpy(titles[0], "Nome: ");
+	strcpy(titles[1], "Idade: ");
+	strcpy(titles[2], "CPF: ");
+	strcpy(titles[3], "Endereco : ");
+	strcpy(titles[4], "Salario : ");
+
+	
+	FILE * file = fopen(fileName, "r");
 	if (!file){
 		printf ("Imposs�vel abrir arquivo");
 	}
-	else{
-		//String respons�vel por armazenar os valores do documento
-		char buffer[1024];	
-		//Enquanto o documento n�o estiver nulo
-		while(fgets(buffer, 1024, file)){
-
-			//Definindo a separa��o quando uma v�rgula for identificada
-			char * value = strtok(buffer, ", ");			
-			//Enquanto aquela linha n�o for nula, percorra
-			while(value){
-				printf("%s  ", value);
-                value = strtok(NULL, ", ");
-			}	
-			//Qubrando entre uma linha e outra
-			printf("\n");
+	
+	char buffer[1000];
+	int control = 0;
+	
+	//Melhoria no printf
+	while (fgets(buffer, sizeof(buffer), file) != NULL) {
+		
+		char * token = strtok(buffer, ",");
+		
+		while (token != NULL) {
+			printf ("%s", titles[control]);
+			control++;
+			printf ("%s\n", token);
+			token = strtok(NULL, ",");
 		}
-		//Encerrando execu��o do arquivo
+		printf("\n\n");
 		fclose(file);
 	}	
 }
 
 void writeFile(Funcionario funcionarios[3] , char nome[25]) {
-    FILE *file = openFIle(nome, "w");
+	
+    FILE * file = openFIle(nome, "r");
+	
+	//Ajuste devido a minha versao do C
+	int c;
 
-    for (int c = 0; c < 3; c++){
+    for (c = 0; c < 3; c++){
         fprintf(file, "%s, %d, %s, %s, %.2f\n",
                 funcionarios[c].nome,
                 funcionarios[c].idade,

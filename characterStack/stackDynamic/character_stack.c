bb489b3c882ed968317b313b000095bb68f2a214
Luiz Fernando <luizfernandoinfo100@gmail.com>
2023-10-23 23:00:00 -0300

    feat: criando função para verificar se a expressão está balanceada.

--- characterStack/stackDynamic/character_stack.c
+++ characterStack/stackDynamic/character_stack.c
@@ -56,3 +56,71 @@ void liberarPilha(Pilha *pilha) {
     }
     free(pilha);
 }
+
+/*
+int estaBalanceada(char *expressao, char *charAbertura, char *charFechamento) {
+    Pilha *pilha = criarPilha();
+    int i = 0;
+
+    while (expressao[i] != '\0') {
+        int c;
+        int ehAbertura = 0;
+
+        for (c = 0; charAbertura[c] != '\0'; c++) {
+            if (expressao[i] == charAbertura[c]) {
+                push(pilha, expressao[i]);
+                ehAbertura = 1;
+                break;
+            }
+        }
+
+        if (!ehAbertura) {
+            for (c = 0; charFechamento[c] != '\0'; c++) {
+                if (expressao[i] == charFechamento[c]) {
+                    if (estaVazia(pilha)) {
+                        return 0;
+                    }
+                    pop(pilha);
+                    break;
+                }
+            }
+        }
+
+        i++;
+    }
+
+    return estaVazia(pilha);
+}
+*/
+
+// Simplificação com o strchr -> realiza comparações, semelhante ao utilizado acima.
+int estaBalanceada(char *expressao) {
+    Pilha *pilha = criarPilha();
+    int i = 0;
+    char *charAbertura = "({[";
+    char *charFechamento = ")}]";
+
+    while (expressao[i] != '\0') {
+        char c = expressao[i];
+
+        if (strchr(charAbertura, expressao[i])) {
+            push(pilha, expressao[i]);
+        } else if (strchr(charFechamento, expressao[i])) {
+            if (estaVazia(pilha)) {
+                liberarPilha(pilha);
+                return 0;
+            }
+            char topo = pop(pilha);
+            if ((c == ')' && topo != '(') || (c == ']' && topo != '[') || (c == '}' && topo != '{')) {
+                liberarPilha(pilha);
+                return 0;
+            }
+        }
+        i++;
+    }
+
+    int resultado = estaVazia(pilha);
+    liberarPilha(pilha);
+    return resultado;
+}
+
